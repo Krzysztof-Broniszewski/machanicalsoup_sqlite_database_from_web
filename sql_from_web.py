8d5bb98fe351f9884a23dac7d4fb4baafde17dee
Krzysztof Broniszewwski <k.broniszewski@gmail.com>
2022-08-20 14:30:03 +0200

    New web to explore

---
+++ sql_from_web.py
@@ -0,0 +1,9 @@
+# https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions
+
+import mechanicalsoup
+import pandas as pd
+import sqlite3
+
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions")
+
